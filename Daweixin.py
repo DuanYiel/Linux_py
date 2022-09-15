e38f44a1093189303cac6cc52e94018bfee2c879
DuanYiel <113496616+DuanYiel@users.noreply.github.com>
2022-09-16 00:40:06 +0800

    Add files via upload
    学号：2020144110 姓名：段怡 第一次作业

---
+++ Daweixin.py
@@ -0,0 +1,19 @@
+import turtle
+from turtle import *
+pendown()
+forward( 200)
+right(120)
+forward(200)
+right(120)
+forward(200)
+penup()
+left(150)
+forward(200*1.44/3)
+pendown()
+left(90)
+forward(200)
+left(120)
+forward(200)
+left(120)
+forward(200)
+
